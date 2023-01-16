c04a455f475d82cce8b8954576efbdd96317272d
Sonk172 <daniel.slachov@gmail.com>
2023-01-16 12:46:58 +1100

    Setup the multi file project

---
+++ main.c
@@ -0,0 +1,15 @@
+// Insertion sort
+//
+// Written by Daniel Slachov
+// on 16/01/2022
+//
+// Sorts an array using the insertion algorithm
+
+#include <stdio.h>
+#include <sorting_helper_functions.h>
+
+int main(void) { 
+  
+
+  return 0; 
+}
